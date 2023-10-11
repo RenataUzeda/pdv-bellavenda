4229d3f29426781812a68fa787337d32f65cb695
SaraDev <sarathaiseg@gmail.com>
2023-10-11 19:45:15 -0300

    feat: validar email já existente

--- src/controladores/usuario-atualizar.js
+++ src/controladores/usuario-atualizar.js
@@ -10,8 +10,9 @@ const atualizarUsuario = async (req, res) => {
     try {
         const usuarioId = req.usuario.id;
 
-        console.log(usuarioId);
+        const emailUnico = await knex('usuarios').where({ email });
 
+        if (emailUnico.length > 0) return res.status(400).json({ mensagem: 'Endereço de e-mail já existe no sistema' });
 
         const senhaCriptografada = await bcrypt.hash(senha, 10);
 
@@ -26,7 +27,7 @@ const atualizarUsuario = async (req, res) => {
         return res.status(201).send({ message: "Usuário atualizado com sucesso" });
 
     } catch (error) {
-        
+
         return res.status(200).json(error.message)
     }
 

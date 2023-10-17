40d52e2802cca1f739c6b0da488e1c2559d00ed8
Marriett Albuquerque <marriettwt@gmail.com>
2023-10-16 23:47:10 -0300

    refact: validação de e-mail existente

--- src/utils/validar-email.js
+++ src/utils/validar-email.js
@@ -1,13 +1,18 @@
 const knex = require('../conexao');
 
-async function validarEmail(email) {
-  
+async function validarEmail(email, usuarioId) {
+
     const emailUnico = await knex('usuarios').where({ email });
 
-    if (emailUnico.length > 0) {
-        throw { statusCode: 400, message: 'Endereço de e-mail já existe no sistema' };
+    if (usuarioId) {
+        if (emailUnico.length > 0 && emailUnico[0].id != usuarioId) {
+            throw { statusCode: 400, message: 'Endereço de e-mail já existe no sistema' };
+        }
+    } else {
+        if (emailUnico.length > 0) {
+            throw { statusCode: 400, message: 'Endereço de e-mail já existe no sistema' };
+        }
     }
-
 }
 
-module.exports = validarEmail;
+module.exports = validarEmail;

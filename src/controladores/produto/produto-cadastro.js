4664aa21e3bb3ee54b50fc4d665f4b2bf7fb8171
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-10-18 17:30:25 -0300

    feat: validar-categoria

--- src/controladores/produto/produto-cadastro.js
+++ src/controladores/produto/produto-cadastro.js
@@ -1,18 +1,15 @@
 const knex = require('../../conexao');
+const validarCategoria = require('../../utils/validar-categoria');
 
 const cadastrarProduto = async (req, res) => {
     const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
     try {
-        if (!descricao || !quantidade_estoque || !valor || !categoria_id) {
-            return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' });
+        if (!descricao || !quantidade_estoque || !valor) {
+            return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios.' });
         }
-        const categoriaExiste = await knex('categorias').where('id', categoria_id);
-        console.log(categoriaExiste)
 
-        if (categoriaExiste.length === 0) {
-            return res.status(404).json({ mensagem: 'A categoria não existe.' });
-        }
+        await validarCategoria(categoria_id);
 
         const novoProduto = {
             descricao,
@@ -21,9 +18,9 @@ const cadastrarProduto = async (req, res) => {
             categoria_id
         }
 
-        //await knex('produtos').insert(novoProduto);
+        await knex('produtos').insert(novoProduto);
 
-        return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso' });
+        return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso.' });
     } catch (error) {
         return res.status(error.statusCode || 500).json({ mensagem: error.message });
     }
